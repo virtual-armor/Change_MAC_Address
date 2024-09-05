a8807f36aa6bfacde7e502594e8c0f3ab9c4a4ac
virtual-armor <virtualinfoarmor@gmail.com>
2024-09-05 18:18:52 -0500

    Format changes

--- change_mac_address.py
+++ change_mac_address.py
@@ -10,7 +10,7 @@ def get_args():
 	parser = op.OptionParser()
 	#call method
 	parser.add_option('-i', '--interface', dest='interface', help='This is to add the interface type, such as eth0')
- 	parser.add_option('-m', '--mac', dest='mac_address', help='This is to enter in the MAC information. Use proper sytax, e.g. 00:11:22:33:44:55. Example: -i eth0 -m 00:11:22:33:44:55')
+	parser.add_option('-m', '--mac', dest='mac_address', help='This is to enter in the MAC information. Use proper sytax, e.g. 00:11:22:33:44:55. Example: -i eth0 -m 00:11:22:33:44:55')
 
 	(options, values) = parser.parse_args()
 	if not options.interface:
@@ -23,9 +23,9 @@ def get_args():
 		return options
 
 def change_mac(interface, mac_address):
-	sp.call(['ifconfig', interface, 'down'])
-	sp.call(['ifconfig', interface, 'hw', 'ether', mac_address])
-	sp.call(['ifconfig', interface, 'up'])
+	sp.call(['sudo', 'ifconfig', interface, 'down'])
+	sp.call(['sudo', 'ifconfig', interface, 'hw', 'ether', mac_address])
+	sp.call(['sudo', 'ifconfig', interface, 'up'])
 	print('\n')
 	sp.call(['ifconfig', interface])
 
