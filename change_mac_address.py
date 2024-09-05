ef296e1e11dc60ccf57948b235e2fff73a1ffc99
virtual-armor <virtualinfoarmor@gmail.com>
2024-09-04 23:03:16 -0500

    Updated Functionality
    Updated function to check user input and handle errors.

--- change_mac_address.py
+++ change_mac_address.py
@@ -13,7 +13,14 @@ def get_args():
  	parser.add_option('-m', '--mac', dest='mac_address', help='This is to enter in the MAC information. Use proper sytax, e.g. 00:11:22:33:44:55. Example: -i eth0 -m 00:11:22:33:44:55')
 
 	(options, values) = parser.parse_args()
-	return options
+	if not options.interface:
+		#handle error
+		parser.error('[-] An interface (e.g. eth0) must be specified.') 
+	elif not options.mac_address:
+		#handle error
+		parser.error('[-] A MAC address must be specified (e.g. 22:11:1a:3b:44:55')
+	else:
+		return options
 
 def change_mac(interface, mac_address):
 	sp.call(['ifconfig', interface, 'down'])
